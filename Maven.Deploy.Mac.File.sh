73c1f36e8215416ffed969368080e221fdd48c4a
ZhengWei <HY.ZhengWei@qq.com>
2022-11-03 09:55:53 +0800

    1.2.0
    1. 添加：将XJava中的日志代理器功能，下沉到本包中，方便为更多的基本包服务

--- Maven.Deploy.Mac.File.sh
+++ Maven.Deploy.Mac.File.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-mvn deploy:deploy-file -Dfile=hy.common.file.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.file/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
-mvn deploy:deploy-file -Dfile=hy.common.file-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.file/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
+mvn install:install-file -Dfile=hy.common.file.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.file/pom.xml
+mvn install:install-file -Dfile=hy.common.file-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.file/pom.xml
+
+mvn deploy:deploy-file   -Dfile=hy.common.file.jar                              -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.file/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
+mvn deploy:deploy-file   -Dfile=hy.common.file-sources.jar -Dclassifier=sources -DpomFile=./src/main/resources/META-INF/maven/cn.openapis/hy.common.file/pom.xml -DrepositoryId=thirdparty -Durl=http://HY-ZhengWei:1481/repository/thirdparty
