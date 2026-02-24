b0221717a5c025689145b341377e2d8bab832e25
HY-ZhengWei <HY.ZhengWei@qq.com>
2026-02-24 15:53:06 +0800

    1.5.8
    1.修改：javax.servlet改为jakarta.servlet
    2.升级：servlet-api从3.0升级为6.1.0

--- src/main/java/org/hy/common/file/FileHelp.java
+++ src/main/java/org/hy/common/file/FileHelp.java
@@ -37,10 +37,10 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
 import javax.imageio.ImageIO;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.hy.common.ByteHelp;
 import org.hy.common.Date;
